2876f9ce28412c43e2229d4a153fcfef71d40e95
Javier Contreras <javierin_kl@hotmail.com>
2018-11-09 07:47:39 -0700

    PantallaGameOver

--- PracticaMovimiento/MainWindow.xaml.cs
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -25,6 +25,10 @@ namespace PracticaMovimiento
     {
         Stopwatch stopwatch;
         TimeSpan tiempoAnterior;
+
+        enum EstadoJuego { Gameplay, Gameover };
+        EstadoJuego estadoActual = EstadoJuego.Gameplay;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,14 +39,14 @@ namespace PracticaMovimiento
 
 
             //1. Establecer instrucciones
-            ThreadStart threadStart = new ThreadStart(moverEnemigos);
+            ThreadStart threadStart = new ThreadStart(actualizar);
             //2. Inicializar el thread
             Thread threadMoverEnemigos = new Thread(threadStart);
             //3. Ejecutar el thread
             threadMoverEnemigos.Start();
         }
 
-        void moverEnemigos()
+        void actualizar()
         {
             while (true)
             {
@@ -51,44 +55,57 @@ namespace PracticaMovimiento
                     {
                         var tiempoActual = stopwatch.Elapsed;
                         var deltaTime = tiempoActual - tiempoAnterior;
-                        double leftViejoActual = Canvas.GetLeft(imgViejo);
-                        Canvas.SetLeft(imgViejo, leftViejoActual - (110 * deltaTime.TotalSeconds));
-                        if(Canvas.GetLeft(imgViejo) <= -100)
+
+                        if(estadoActual == EstadoJuego.Gameplay)
                         {
-                            Canvas.SetLeft(imgViejo, 800);
+                            double leftViejoActual = Canvas.GetLeft(imgViejo);
+                            Canvas.SetLeft(imgViejo, leftViejoActual - (110 * deltaTime.TotalSeconds));
+                            if (Canvas.GetLeft(imgViejo) <= -100)
+                            {
+                                Canvas.SetLeft(imgViejo, 800);
+                            }
+
+                            //interseccion en X
+                            double xViejo = Canvas.GetLeft(imgViejo);
+                            double xDinosaurio = Canvas.GetLeft(imgDinosaurio);
+                            if (xDinosaurio + imgDinosaurio.Width >= xViejo && xDinosaurio <= xViejo + imgViejo.Width)
+                            {
+                                lblInterseccionX.Text = "SI HAY COLISIÓN EN X!!!";
+                            }
+                            else
+                            {
+                                lblInterseccionX.Text = "No hay intersección en X";
+                            }
+                            //en y
+                            double yViejo = Canvas.GetTop(imgViejo);
+                            double yDinosaurio = Canvas.GetTop(imgDinosaurio);
+                            if (yDinosaurio + imgDinosaurio.Height >= yViejo && yDinosaurio <= yViejo + imgViejo.Height)
+                            {
+                                lblInterseccionY.Text = "SI HAY INTERSECCION EN Y";
+                            }
+                            else
+                            {
+                                lblInterseccionY.Text = "No hay intersección en Y";
+                            }
+
+                            if (xDinosaurio + imgDinosaurio.Width >= xViejo && xDinosaurio <= xViejo + imgViejo.Width && yDinosaurio + imgDinosaurio.Height >= yViejo && yDinosaurio <= yViejo + imgViejo.Height)
+                            {
+                                lblColision.Text = "Eh we, ya te mataron";
+                                estadoActual = EstadoJuego.Gameover;
+                                miCanvas.Visibility = Visibility.Collapsed;
+                                canvasGameOver.Visibility = Visibility.Visible;
+                            }
+                            else
+                            {
+                                lblColision.Text = "No hay colisión";
+                            }
+                        }
+                        else if(estadoActual == EstadoJuego.Gameover)
+                        {
+
                         }
 
-                        //interseccion en X
-                        double xViejo = Canvas.GetLeft(imgViejo);
-                        double xDinosaurio = Canvas.GetLeft(imgDinosaurio);
-                        if(xDinosaurio + imgDinosaurio.Width >= xViejo && xDinosaurio <= xViejo + imgViejo.Width )
-                        {
-                           lblInterseccionX.Text = "SI HAY COLISIÓN EN X!!!";
-                        }
-                        else
-                        {
-                            lblInterseccionX.Text = "No hay intersección en X";
-                        }
-                        //en y
-                        double yViejo = Canvas.GetTop(imgViejo);
-                        double yDinosaurio = Canvas.GetTop(imgDinosaurio);
-                        if(yDinosaurio + imgDinosaurio.Height >= yViejo && yDinosaurio <= yViejo + imgViejo.Height)
-                        {
-                            lblInterseccionY.Text = "SI HAY INTERSECCION EN Y";
-                        }
-                        else
-                        {
-                            lblInterseccionY.Text = "No hay intersección en Y";
-                        }
-
-                        if(xDinosaurio + imgDinosaurio.Width >= xViejo && xDinosaurio <= xViejo + imgViejo.Width && yDinosaurio + imgDinosaurio.Height >= yViejo && yDinosaurio <= yViejo + imgViejo.Height)
-                        {
-                            lblColision.Text = "Eh we, ya te mataron";
-                        }
-                        else
-                        {
-                            lblColision.Text = "No hay colisión";
-                        }
+                        
 
                             tiempoAnterior = tiempoActual;
                     }
@@ -98,25 +115,28 @@ namespace PracticaMovimiento
 
         private void miCanvas_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key== Key.Up)
-            {
-                double topDinoActual = Canvas.GetTop(imgDinosaurio);
-                Canvas.SetTop(imgDinosaurio, topDinoActual - 15);
-            }
-            if (e.Key == Key.Down)
-            {
-                double topDinoActual = Canvas.GetTop(imgDinosaurio);
-                Canvas.SetTop(imgDinosaurio, topDinoActual + 15);
-            }
-            if (e.Key == Key.Left)
-            {
-                double rightDinoActual = Canvas.GetLeft(imgDinosaurio);
-                Canvas.SetLeft(imgDinosaurio, rightDinoActual - 15);
-            }
-            if (e.Key == Key.Right)
-            {
-                double rightDinoActual = Canvas.GetLeft(imgDinosaurio);
-                Canvas.SetLeft(imgDinosaurio, rightDinoActual + 15);
+            if(estadoActual == EstadoJuego.Gameplay)
+            { 
+                if(e.Key== Key.Up)
+                {
+                    double topDinoActual = Canvas.GetTop(imgDinosaurio);
+                    Canvas.SetTop(imgDinosaurio, topDinoActual - 15);
+                }
+                if (e.Key == Key.Down)
+                {
+                    double topDinoActual = Canvas.GetTop(imgDinosaurio);
+                    Canvas.SetTop(imgDinosaurio, topDinoActual + 15);
+                }
+                if (e.Key == Key.Left)
+                {
+                    double rightDinoActual = Canvas.GetLeft(imgDinosaurio);
+                    Canvas.SetLeft(imgDinosaurio, rightDinoActual - 15);
+                }
+                if (e.Key == Key.Right)
+                {
+                    double rightDinoActual = Canvas.GetLeft(imgDinosaurio);
+                    Canvas.SetLeft(imgDinosaurio, rightDinoActual + 15);
+                }
             }
         }
     }
